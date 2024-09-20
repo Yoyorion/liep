189aaf245b1bffa3a2a9e9ade968c1556bbb0adb
Yoyorion <129601124+Yoyorion@users.noreply.github.com>
2024-09-20 19:19:15 +0200

    Update calendar_script.js

--- calendar_script.js
+++ calendar_script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialisation de Supabase
     const supabaseUrl = 'https://znwzdkgshtrickigthgd.supabase.co';  // Remplace par ton URL Supabase
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpud3pka2dzaHRyaWNraWd0aGdkIiwicm9zZSI6ImFub24iLCJpYXQiOjE3MjY4MjQyMzcsImV4cCI6MjA0MjQwMDIzN30.qGSSUfV7qjC0PUL3t_XVR3dXg6s5kRg0zwtQ2J1Gd5M';  // Remplace par ton anonpublic key
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpud3pka2dzaHRyaWNraWd0aGdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4MjQyMzcsImV4cCI6MjA0MjQwMDIzN30.qGSSUfV7qjC0PUL3t_XVR3dXg6s5kRg0zwtQ2J1Gd5M';  // Remplace par ton anonpublic key
     const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
     // Récupérer et afficher l'emploi du temps depuis la base de données
