a66ab771aa421dbf7314bf4380147bdc653160c6
Yoyorion <129601124+Yoyorion@users.noreply.github.com>
2024-10-13 07:22:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Script pour gérer les bulles et autres interactions sur la page d'accueil
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Page d'accueil chargée avec succès !");
-    // Ajoute ici des interactions supplémentaires si nécessaire
-});
